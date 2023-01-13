4200f634e43ae8e9b7dc10addbe75ddcea2f3752
Jeremias <jeremias.elian@gmail.com>
2023-01-13 02:03:00 -0300

    Preparacion para subir a la nube

--- src/main/java/com/backendportfolio/portfolioBackend/controller/ContactController.java
+++ src/main/java/com/backendportfolio/portfolioBackend/controller/ContactController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping ("/api")
 public class ContactController {
--- src/main/java/com/backendportfolio/portfolioBackend/controller/ProjectController.java
+++ src/main/java/com/backendportfolio/portfolioBackend/controller/ProjectController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping ("/api")
 public class ProjectController {
